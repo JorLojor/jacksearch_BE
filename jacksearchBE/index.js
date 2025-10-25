4358894fdf617d628297621ee49b944e05061487
george ibrahim hanif <122005621+JorLojor@users.noreply.github.com>
2025-10-26 03:23:28 +0700

    seeeeee

--- jacksearchBE/index.js
+++ jacksearchBE/index.js
@@ -1,10 +1,9 @@
 const app = require("./app");
 require("dotenv").config();
 
-const PORT = process.env.PORT;
-const HOST = process.env.HOST;
-
+const PORT = process.env.PORT || 5434;
+const HOST = process.env.HOST || "0.0.0.0";
 
 app.listen(PORT, HOST, () => {
-    console.log(`POS running on: http://${HOST}:${PORT}`);
+    console.log(`API running on: http://${HOST}:${PORT}`);
 });
